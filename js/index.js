6dffe8cecd7b5492caf4cb2581547de6bae913fe
Roman Fedusiw <romanfedusiv@gmail.com>
2019-07-24 17:24:27 +0000

    push

---
+++ js/index.js
@@ -0,0 +1,24 @@
+alert('Hello Java Script');
+
+let userName = prompt('Enter your name');
+let greetings = 'Hello:'
+alert(greetings+userAge);
+
+let userAge = prompt('Enter your age');
+alert(userAge);
+let nextAge= userAge + 1;
+
+if(userAge >=18) { // < > <= >= == ===!= !== ||
+alert('Ви ще неповнолытній');
+} else {
+}
+
+function add(a, b) {
+    let result
+
+}
+
+
+
+
+
